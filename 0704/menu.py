dc2ac4307c95d24a219f586945e2afdd54e85fc9
kkura <flkuratomijr@gmail.com>
2026-04-12 15:31:08 -0300

    changes

--- 0704/menu.py
+++ 0704/menu.py
@@ -25,37 +25,37 @@ while True:
             print("Elemento removido:", removido)
 
 
-list_especf = input("\nDeseja remover um valor especifico?(s/n): ")
-if list_especf == "s":
-    remover = input("\nDigite o valor que queria remover: ")
-    if remover in lista:
-        lista.remove(remover)
-        print("Valor removido com sucesso.")
+    list_especf = input("\nDeseja remover um valor especifico?(s/n): ")
+    if list_especf == "s":
+        remover = input("\nDigite o valor que queria remover: ")
+        if remover in lista:
+            lista.remove(remover)
+            print("Valor removido com sucesso.")
+        else:
+            print("Valor não encontrado na lista.")
+    elif list_especf == "n":
+        print("Nenhum valor foi removido")
+    print("\nLista:", lista)
+
+
+    busca = input("\nDigite um valor para buscar posição: ")
+    if busca in lista:
+        posicao = lista.index(busca)  # retorna o índice
+        print("O valor está na posição:", posicao)
     else:
-        print("Valor não encontrado na lista.")
-elif list_especf == "n":
-    print("Nenhum valor foi removido")
-print("\nLista:", lista)
+        print("Valor não encontrado.")
+    print("\nLista:", lista)
 
+    valor_contar = input("\nDigite um valor para contar: ")
+    quantidade = lista.count(valor_contar)
+    print("O valor aparece", quantidade, "vez(es).")
+        
+    lista.sort()
+    print("\nLista ordenada:", lista)
 
-busca = input("\nDigite um valor para buscar posição: ")
-if busca in lista:
-    posicao = lista.index(busca)  # retorna o índice
-    print("O valor está na posição:", posicao)
-else:
-    print("Valor não encontrado.")
-print("\nLista:", lista)
+    lista.reverse()
+    print("Lista invertida:", lista)
 
-valor_contar = input("\nDigite um valor para contar: ")
-quantidade = lista.count(valor_contar)
-print("O valor aparece", quantidade, "vez(es).")
-    
-lista.sort()
-print("\nLista ordenada:", lista)
+    sair = input("\nPara sair do sistema digite '.' ")
 
-lista.reverse()
-print("Lista invertida:", lista)
-
-sair = input("\nPara sair do sistema digite '.' ")
-
-    
+        
